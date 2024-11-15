4da340dad3e5b17ba7144b7238679ef05a7e9750
kallilriquelme <kallriq@gmail.com>
2024-11-15 18:08:46 -0400

    1.0

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-console.log("Ola Mundo")
-//ghvghhghbbn
-//sadasda
-//ksdnfklsdnklsnln
+let nomeHeroi = "Arthur"
+let numeroDeBatalhas = 1000
+let xpHeroi = 0
+let nivelHeroi = ""
+let expBatalha = 15
+
+for (xpHeroi = 0; xpHeroi<(expBatalha * numeroDeBatalhas); xpHeroi++) {
+    
+console.log(xpHeroi)
+
+if (xpHeroi<=1000){nivelHeroi = "Ferro"}
+else if(1001<=xpHeroi && xpHeroi<=2000){nivelHeroi = "Bronze"}
+else if(2001<=xpHeroi && xpHeroi<=5000){nivelHeroi = "Prata "}
+else if(5001<=xpHeroi && xpHeroi<=7000){nivelHeroi = "Ouro"}
+else if(7001<=xpHeroi && xpHeroi<=8000){nivelHeroi = "Platina"}
+else if(8001<=xpHeroi && xpHeroi<=9000){nivelHeroi = "Ascendente"}
+else if(9001<=xpHeroi && xpHeroi<=10000){nivelHeroi = "Imortal"}
+else if(xpHeroi>=10001){nivelHeroi = "Radiante"}
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
